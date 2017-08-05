51fb791d050f0097583cf1a77e3590aa341ef26e
Stephany Choi <s.choi417@gmail.com>
2017-08-04 20:24:03 -0400

    Removed second angular.module in modal control

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,30 +1,28 @@
 (function() {
+  function ModalCtrl($uibModal, $log, $document, Room, $uibModalInstance) {
+    var $ctrl = this;
+    $ctrl.open = function () {
+      $uibModal.open({
+        templateUrl: '/templates/room_form.html',
+        controller:   function($uibModalInstance, Room){
+            var $ctrl = this;
+
+            $ctrl.ok = function () {
+              var name = document.getElementById("new-room-name").value;
+              Room.add();
+              $uibModalInstance.close(name);
+            };
+
+            $ctrl.cancel = function () {
+              $uibModalInstance.dismiss('cancel');
+            };
+          },
+        controllerAs: '$ctrl'
+      });
+    };
+  }
+
   angular
     .module('blocChat',['ui.bootstrap'])
-    .controller('ModalCtrl', ['$uibModal','$log','$document', function ($uibModal, $log, $document) {
-      var $ctrl = this;
-      $ctrl.open = function () {
-        $uibModal.open({
-          templateUrl: '/templates/room_form.html',
-          controller: 'ModalInstanceCtrl',
-          controllerAs: '$ctrl'
-        });
-    };
-  }]);
-
-  angular
-    .module('blocChat', ['ui.bootstrap'])
-    .controller('ModalInstanceCtrl', ['$uibModalInstance','Room', function ($uibModalInstance, Room) {
-      var $ctrl = this;
-
-      $ctrl.ok = function () {
-        var name = document.getElementById("new-room-name").value;
-        Room.add();
-        $uibModalInstance.close(name);
-      };
-
-      $ctrl.cancel = function () {
-        $uibModalInstance.dismiss('cancel');
-      };
-  }]);
+    .controller('ModalCtrl', ['$uibModal','$log','$document','Room', '$uibModalInstance', ModalCtrl]);
 })();
