25f345b882b9580ee8511a6e265d6ba072669635
Stephany Choi <s.choi417@gmail.com>
2017-08-11 09:36:26 -0400

    Adjust timecode filter

--- app/scripts/timecode.js
+++ app/scripts/timecode.js
@@ -1,7 +1,7 @@
 (function() {
   function timecode() {
     return function(unix) {
-      var seconds = Number.parseFloat(unix);
+      var seconds = Number.parseFloat(unix*1000);
       return moment(seconds).format('h:mm a');
     };
   }
